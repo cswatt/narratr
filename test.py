798f1790bf68a116bd452fbcae57dc7bea25fe7e
cswatt <ceciliawatt@gmail.com>
2015-03-24 11:27:34 -0400

    just making lexer and parser pep8 into different tests

--- test.py
+++ test.py
@@ -4,9 +4,16 @@ import pep8
 
 class TestCodeFormat(unittest.TestCase):
 
-    def test_pep8_conformance(self):
-        """Test that we conform to PEP8."""
+    def test_pep8_conformance_lexer(self):
+        """Test that lexer conforms to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['lexer.py', 'parser.py'])
+        result = pep8style.check_files(['lexer.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pep8_conformance_parser(self):
+        """Test that parser conforms to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['parser.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
