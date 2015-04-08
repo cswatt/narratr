1f331f60c3ca41c17bccb9d77cdc1f4a247b412e
cswatt <ceciliawatt@gmail.com>
2015-04-08 19:16:20 -0400

    added tests for pep8 conformance in tests!! and then made tests actually conform to pep8. also, set quiet=False for better feedback on failing tests

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -1,27 +1,29 @@
 from lexer import LexerForNarratr
 import unittest
 
+
 class TestLexer(unittest.TestCase):
 
-	def test_lexer_helloworld(self):
-		"""Test that lexer can read Hello World."""
-		# some lists
-		correctlist = []
-		tokenlist = []
+    def test_lexer_helloworld(self):
+        """Test that lexer can read Hello World."""
+        # some lists
+        correctlist = []
+        tokenlist = []
 
-		# look at correct list of tokens
-		with open('tests/helloworld_tokens') as f:
-			for line in f:
-				correctlist.append(line.rstrip())
+        # look at correct list of tokens
+        with open('tests/helloworld_tokens') as f:
+            for line in f:
+                correctlist.append(line.rstrip())
 
-		# look at what lexer outputs
-		with open('sampleprograms/helloworld.ntr') as f:
-			m = LexerForNarratr()
-			m.input(f.read())
-			t = m.token()
-			while t:
-				tokenlist.append(str(t))
-				t = m.token()
+        # look at what lexer outputs
+        with open('sampleprograms/helloworld.ntr') as f:
+            m = LexerForNarratr()
+            m.input(f.read())
+            t = m.token()
+            while t:
+                tokenlist.append(str(t))
+                t = m.token()
 
-		# check for equivalence
-		self.assertEqual(cmp(tokenlist, correctlist), 0, "Something did not work correctly.")
+        # check for equivalence
+        self.assertEqual(cmp(tokenlist, correctlist), 0,
+                         "Something did not work correctly.")
--- tests/test_pep8.py
+++ tests/test_pep8.py
@@ -6,28 +6,42 @@ class TestCodeFormat(unittest.TestCase):
 
     def test_pep8_conformance_lexer(self):
         """Test that lexer conforms to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
+        pep8style = pep8.StyleGuide(quiet=False)
         result = pep8style.check_files(['lexer.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_pep8_conformance_parser(self):
         """Test that parser conforms to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
+        pep8style = pep8.StyleGuide(quiet=False)
         result = pep8style.check_files(['parser.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_pep8_conformance_codegen(self):
         """Test that codegen conforms to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
+        pep8style = pep8.StyleGuide(quiet=False)
         result = pep8style.check_files(['codegen.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_pep8_conformance_node(self):
         """Test that node conforms to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
+        pep8style = pep8.StyleGuide(quiet=False)
         result = pep8style.check_files(['node.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pep8_conformance_lexertest(self):
+        """Test that lexer test conforms to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=False)
+        result = pep8style.check_files(['tests/test_lexer.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pep8_conformance_pep8test(self):
+        """Test that pep8 test conforms to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=False)
+        result = pep8style.check_files(['tests/test_pep8.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
