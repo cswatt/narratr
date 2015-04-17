c825ef9011ce585a11b0d32127a42b9948bde738
hylbyj <hyldestiny@gmail.com>
2015-04-17 10:34:34 -0400

    new process function without DFS

--- codegen.py
+++ codegen.py
@@ -1,21 +1,3 @@
-# -----------------------------------------------------------------------------
-# narrtr: codegen.py
-# This file defines the CodeGen class for narratr games.
-#
-# Copyright (C) 2015 Team narratr
-# All Rights Reserved
-# Team narratr: Yelin Hong, Shloka Kini, Nivvedan Senthamil Selvan, Jonah
-# Smith, Cecilia Watt
-#
-# File Created: 1 April 2015
-# Primary Author: Jonah Smith
-#
-# Any questions, bug reports and complaints are to be directed at the primary
-# author.
-#
-# -----------------------------------------------------------------------------
-
-
 class CodeGen:
     def __init__(self):
         self.frontmatter = "#!/usr/bin/env python\nimport sys\n"
@@ -24,27 +6,42 @@ class CodeGen:
         self.items = []
         self.main = ""
 
-    # This function takes a node and sets the processing of the source in
-    # motion. It takes as input the root node of the abstract syntax tree.
-    # It is meant to be called externally (by the main compiler) and should
-    # be called first before construct.
-    # The general strategy: do dfs and process all of the sceneblocks [and
-    # eventually itemblocks]. Also find the startstates and use them to
-    # construct main. Add to the appropriate instance variables.
+    # This function iterates through all the nodes of the tree since we know 
+    # the structure already 
     def process(self, node):
-        if node.type == "scene_block":
-            self._add_scene(self._scene_gen(node))
+        for c in node.children:
+            if c.type == "blocks":
+                for bc in c.children:
+                    if type (bc) is dict:
+                        for key, s_i in bc.iteritems():
+                            if s_i.type == "scene_block":
+                                print "scene" 
+                                self._add_scene(self._scene_gen(s_i,key))
+                            elif s_i.type == "item_block":
+                                pass
+                    elif bc.type == "start_state":
+                        self._add_main(bc)    
+
+                        
+
+
+
+
+
+
+    #    if node.type == "scene_block":
+    #        self._add_scene(self._scene_gen(node))
 
         # This is just a boilerplate example, it's not implemented yet.
-        if node.type == "item_block":
-            self._item_gen(node)
+    #    if node.type == "item_block":
+    #        self._item_gen(node)
 
-        elif node.type == "startstate":
-            self._add_main(node)
+    #    elif node.type == "startstate":
+    #        self._add_main(node)
 
-        if not node.is_leaf():
-            for n in node.children:
-                self.process(n)
+    #    if not node.is_leaf():
+    #        for n in node.children:
+    #            self.process(n)
 
     # This function takes the instance variables constructed by the process()
     # function and writes them to an output file. (As such, it must be run
@@ -107,15 +104,17 @@ class CodeGen:
     # valid Python (really, a Python class). Iterates through the children
     # of the input node and constructs the setup, cleanup, and action blocks
     # using boilerplate code. This should only be used internally.
-    def _scene_gen(self, scene):
+    def _scene_gen(self, scene,sid):
         commands = []
         for c in scene.children:
-            if c.type == "sceneid":
+            print c
+            if c.type == "SCENEID":
                 sid = c.value
 
             elif c.type == "setup_block":
                 commands.append("def setup(self):\n" +
                                 self._process_statements(c.children, 2) +
+                                "\n        print 'hello word'\n"
                                 "\n        self.action()\n")
 
             elif c.type == "cleanup_block":
