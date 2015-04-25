1dbe7427b16f0ea56c9d15ddd0d467b5354f85a9
Cecilia Watt <ceciliawatt@gmail.com>
2015-04-25 18:47:00 -0400

    added blank init files, sorting out import bug in parser test

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,19 +1,18 @@
-from parser import ParserForNarratr
+import narratr.parser as parser
 import unittest
 
 
 class TestParser(unittest.TestCase):
 
 	def test_parser_helloworld(self):
+		
 		"""Test that parser can parse helloworld."""
 		ast = ""
 		correct_ast = ""
 		with open('tests/parsed_helloworld') as f:
 			correct_ast = f.read()
-		p = ParserForNarratr()
+
+		p = parser.ParserForNarratr()
 		with open('sampleprograms/helloworld.ntr') as f:
 			ast = str(p.parse(f.read()))
 		self.assertEqual(ast, correct_ast, "ruh roh")
-
-if __name__ == "__main__":
-	unittest.main()
