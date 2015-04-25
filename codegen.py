869f939004d392cf0953b3a5a7aabf8e9fdb4b5d
hylbyj <hyldestiny@gmail.com>
2015-04-25 03:38:12 -0400

    Rough codegen without if
    Can you guys take a look at this codegen? Several things update:
    1. Since the if statement is not ready in the parser.py, I can’t test
    my code now.
    2. I made the direction as a dictionary, so I have to change the
    interface of action()
    3. The response has some indent issue. not sure why

--- codegen.py
+++ codegen.py
@@ -82,7 +82,7 @@ class CodeGen:
                 response = raw_input(" -->> ")
                 response = response.lower()
                 response = response.translate(None,
-                            '!#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~')
+                            "!#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~")
                 response = ' '.join(response.split())
                 if response == "exit":
                     print "== GAME TERMINATED =="
@@ -116,32 +116,48 @@ class CodeGen:
     # using boilerplate code. This should only be used internally.
     def _scene_gen(self, scene, sid):
         commands = []
+        direction_sign = False
         for c in scene.children:
             if c.type == "SCENEID":
                 sid = c.value
 
             elif c.type == "setup_block":
-                commands.append("def setup(self):\n" +
-                                self._process_statements(c.children, 2) +
-                                "\n        print 'hello word'\n"
-                                "\n        self.action()\n")
+                #add suite block node here
+                commands.append("def setup(self):\n")
+                if len(c.children) > 0:
+                    for child in c.children:
+                        commands.append(self._process_statements(child, 1))
+                        direction_sign = self._process_detect_direction(child)
+                commands.append("\n        self.action(" + ("direction" if (direction_sign) else "") + ")\n")
 
             elif c.type == "cleanup_block":
-                commands.append("def cleanup(self):\n        pass\n" +
-                                self._process_statements(c.children, 2))
+                commands.append("def cleanup(self):\n        pass\n") 
+                if len(c.children) > 0:
+                    commands.append(self._process_statements(c.children[0], 2))
 
             elif c.type == "action_block":
-                commands.append("def action(self):\n        " +
-                                "response = \"\"\n        while(True):\n" +
-                                self._process_statements(c.children, 3) +
-                                "\n            response = get_response()\n")
+                commands.append("def action(self" + (" , direction" if (direction_sign) else "") + "):\n")
+                if len(c.children) > 0:
+                    for child in c.children:
+                        commands.append(self._process_statements(child, 2))
+                #                "response = \"\"\n        while(True):\n" +
+                #                self._process_statements(c.children[0], 3) +
+                #                "\n            response = get_response()\n")
 
         self.scene_nums.append(sid)
-
         scene_code = "class s_" + str(sid) + ":\n    def __init__(self):"\
             + "\n        pass\n\n    " + "\n    ".join(commands)
 
         return scene_code
+
+    #Takes a "suite" node, calls the function to process "statemant"    
+    def _process_suite(self , suite , indentlevel=1):
+        commands = []
+        if len(suite.children) > 0:
+            for smt in suite.children:
+                if smt.type == "statement":
+                    commands.append(self._process_statements(smt, 2))
+        return commands
 
     # Takes a "statements" node, calls a function to find the "statement"
     # nodes that descend from it, and then figures out what kind of statement
@@ -151,40 +167,120 @@ class CodeGen:
     # node. This function should only be used internally. Indent level
     # specifies how many indents should appear before statements in the
     # sequence.
-    def _process_statements(self, statements, indentlevel=1):
-        # returns a list of statement nodes
-        smts = self._find_statement(statements)
-        commands = []
+    def _process_statements(self, statement, indentlevel=1):
+        commands = ''
         prefix = "\n" + "    "*indentlevel
+        indent = 1
+        for smt in statement.children:
+            if smt.value == "say":
+                commands += "    "*indentlevel + "print "
+                for testlist in smt.children:
+                    commands += self._process_testlist(testlist, 2) + "\n"
+            elif smt.value == "exposition":
+                indent += 1
+                commands += "    "*indentlevel + "print "
+                for testlist in smt.children:
+                    commands += self._process_testlist(testlist, 2) + "\n"
 
-        for smt in smts:
-            command = smt.children[0]
-            if command.type == "say":
-                commands.append("print \"" +
-                                command.children[0].value + "\"")
+            elif smt.value == "win":
+                print "win!!!!!"
+                print smt
+                if len(smt.children) > 0:
+                    for testlist in smt.children:
+                        self._process_testlist(testlist, 2)
 
-            if command.type in ["win", "lose"]:
-                # win with an argument
-                if len(command.children) > 0:
-                    commands.append("print \"" +
-                                    command.children[0].value +
-                                    "\"")
-                commands.append("exit(0)")
-        if len(commands) > 0:
-            commands[0] = "    "*indentlevel + commands[0]
-        return prefix.join(commands)
+            elif smt.value == "expression":
+                print "match the expression"
 
-    # This function finds all the "statement" children of a "statements" node.
-    # It uses a non-recursive DFS algorithm, basically. It returns a list of
-    # nodes, IN ORDER OF APPEARANCE. To be used internally.
-    def _find_statement(self, statements):
-            smts = []
-            nodes_to_visit = statements
-            while len(nodes_to_visit) > 0:
-                cnode = nodes_to_visit.pop(0)
-                if cnode.type == "simple_statement":
-                    smts.insert(0, cnode)
-                else:
-                    for node in cnode.children:
-                        nodes_to_visit.insert(0, node)
-            return smts
+            elif smt.value == "flow":
+                commands += "    "*indent
+                if len(smt.children) > 0:
+                    for child in smt.children:
+                        i = 0
+                        if child.type == "direction":
+                            commands += 'direction = {"'
+                            commands += self._process_direction(child, 2)
+                            if i != len(smt.children) - 1:
+                                commands += ', '
+                            else:
+                                commands += "}\n"
+            
+            elif smt.value == "lose":
+                if len(smt.children) > 0:
+                    for testlist in smt.children:
+                        self._process_testlist(testlist, 2)
+
+            elif smt.value == None:
+                commands += self._process_testlist(smt, 2)
+                #commands.append("exit(0)")
+                #commands.append("sys.exit(0)")
+
+        return commands
+
+
+    def _process_detect_direction(self, statement):
+        for smt in statement.children:
+            if smt.value == "flow":
+                if len(smt.children) > 0:
+                    for child in smt.children:
+                        if child.type == "direction":
+                            return True
+        return False
+
+    #This function takes "testlist" node as argument
+    def _process_testlist(self, testlist, indentlevel=1):
+        commands = ''
+
+        for test in testlist.children:
+            if test.type == "test":
+                for child in test.children:
+                    if child.type == "expression":
+                        commands += self._process_expression(child, 2)
+                    if child.type == "and_test":
+                        commands += self._process_expression(child, 2)
+                    if child.type == "not_test":
+                        commands += self._process_expression(child, 2)
+                    if child.type == "or_test":
+                        commands += self._process_expression(child, 2)
+            elif test.type == "suite":
+                commands += "    "*3 + "win"
+        return commands
+
+    #This function takes "expression" node as argument
+    def _process_expression(self, exps, indentlevel=1):
+        commands = ''
+        
+        for child in exps.children:
+            if child.type == "factor":
+                commands += '"' + child.value + '"'
+            if child.type == "expression":
+                if len(child.children) > 0:
+                    for exex in child.children:
+                        if exex.type == "factor":
+                            commands += "\n" + "    "*2 + self._process_factor(exex, 2) + "\n"
+        return commands
+
+    #This function takes "factor" node as argument
+    def _process_factor(self, factors, indentlevel=2):
+        commands = ""
+        if len(factors.children) > 0:
+            if len(factors.children) == 3:
+                for factor in factors.children[0].children:
+                    commands += str(factor.value)
+                commands += factors.children[1].value
+                for factor in factors.children[2].children:
+                    commands += str(factor.value)
+        return commands
+
+
+    #This function takes "direction" node as argument
+    #Building a dictionary for direction, using the direction as key and scene no as value
+    def _process_direction(self, direction, indentlevel=1):
+        commands = ''
+        commands += direction.value
+        commands += '": '
+        for scene in direction.children:
+            commands += str(scene.value)
+        return commands
+
+
