40ab755b63f8d747e3d2a6a67614bb2907b47f13
cswatt <ceciliawatt@gmail.com>
2015-04-27 16:36:07 -0400

    reorganized lexer tests, added parser tests for current sample programs. looks like parser isnt dealing with if-statements right now

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,8 +5,8 @@ import unittest
 class TestParser(unittest.TestCase):
 
 	def test_parser_helloworld(self):
-		
-		"""Test that parser can parse helloworld."""
+		"""Test that parser can parse Hello World."""
+		"""Test that AST for Hello World is as expected."""
 		ast = ""
 		correct_ast = ""
 		with open('tests/parsed_helloworld') as f:
@@ -15,4 +15,57 @@ class TestParser(unittest.TestCase):
 		p = parser.ParserForNarratr()
 		with open('sampleprograms/helloworld.ntr') as f:
 			ast = str(p.parse(f.read()))
-		self.assertEqual(ast, correct_ast, "ruh roh")
+		self.assertEqual(ast, correct_ast, 
+						 "Hello World not parsed as expected")
+
+	def test_parser_andor(self):
+		
+		"""Test that parser can parse and/or."""
+		ast = ""
+
+		p = parser.ParserForNarratr()
+		with open('sampleprograms/andor.ntr') as f:
+			ast = str(p.parse(f.read()))
+
+	def test_parser_elseif(self):
+		
+		"""Test that parser can parse else/if."""
+		ast = ""
+
+		p = parser.ParserForNarratr()
+		with open('sampleprograms/elseif.ntr') as f:
+			ast = str(p.parse(f.read()))
+
+	def test_parser_exposition(self):
+		
+		"""Test that parser can parse exposition."""
+		"""Test that AST for exposition is as expected."""
+		ast = ""
+		correct_ast = ""
+		with open('tests/parsed_exposition') as f:
+			correct_ast = f.read()
+
+		p = parser.ParserForNarratr()
+		with open('sampleprograms/exposition.ntr') as f:
+			ast = str(p.parse(f.read()))
+		self.assertEqual(ast, correct_ast, 
+						 "Exposition not parsed as expected")
+
+	def test_parser_if(self):
+		
+		"""Test that parser can parse if."""
+		ast = ""
+
+		p = parser.ParserForNarratr()
+		with open('sampleprograms/if.ntr') as f:
+			ast = str(p.parse(f.read()))
+
+	def test_parser_truefalse(self):
+		
+		"""Test that parser can parse true/false."""
+		ast = ""
+
+		p = parser.ParserForNarratr()
+		with open('sampleprograms/truefalse.ntr') as f:
+			ast = str(p.parse(f.read()))
+			
